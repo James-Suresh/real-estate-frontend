61f87b81671d8924ab91733261952dc12cf32b79
James-Suresh <jamesblesson@gmail.com>
2023-05-18 10:51:40 -0400

    Hero complete

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Hero from './components/Hero.vue'
+//import Listings from './components/Listings.vue'
 </script>
 <template>
   <div>
     <Hero/>
+    <!-- <Listings/> -->
   </div>
 </template>
---
+++ src/components/Hero.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="hero">
+      <!-- Content of your hero section -->
+    
+        <div class="search">
+            <input placeholder="Enter address" class="search-field" type="text">
+            <button class="btn">Search</button>
+        </div>
+</div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+</script>
+
+<style scoped>
+.hero{
+    display: flex;
+    justify-content: center;
+    background-image: url("src/assets/real-estate-bg.jpg");
+    background-color: aqua;
+    align-items: center;
+    background-position: center;
+    background-size: cover;
+    height: 30rem;
+    width: 100vw;
+    margin: 0;
+}
+.search{
+    display: flex;
+    height: 5rem;
+    gap: 1rem;
+    align-items: center;
+    padding: 2rem;
+    width: 50vw;
+    background-color: rgba(0, 0, 0, 0.3);
+}
+.search-field{
+    width: 100%;
+    height: 2rem;
+}
+
+.btn {
+    background-color: rebeccapurple;
+    
+    height: 2rem;
+    color: #fff;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    cursor: pointer;
+    border: 2px dashed rebeccapurple;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+    transition: .4s;
+   }
+   
+   .btn span:last-child {
+    display: none;
+   }
+   
+   .btn:hover {
+    transition: .4s;
+    border: 2px dashed rebeccapurple6;
+    background-color: #fff;
+    color: rebeccapurple;
+   }
+   
+   .btn:active {
+    background-color: #87dbd0;
+   }
+  
+</style>
